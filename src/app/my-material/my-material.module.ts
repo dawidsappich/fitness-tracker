078763368ef62606078c77bdc7477792d3a048e0
dawid sappich <dawidsappich@gmail.com>
2019-01-20 19:33:04 +0100

    style toolbar and sidenav, add icons to sidenav

--- src/app/my-material/my-material.module.ts
+++ src/app/my-material/my-material.module.ts
@@ -1,20 +1,24 @@
 import {NgModule} from '@angular/core';
 import {
-  MatButtonModule, MatCheckboxModule,
+  MatButtonModule,
+  MatCheckboxModule,
   MatDatepickerModule,
   MatFormFieldModule,
   MatIconModule,
   MatInputModule,
-  MatNativeDateModule, MatSidenavModule, MatToolbarModule
+  MatListModule,
+  MatNativeDateModule,
+  MatSidenavModule,
+  MatToolbarModule
 } from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule,
-    MatSidenavModule, MatToolbarModule
+    MatSidenavModule, MatToolbarModule, MatListModule
   ],
   exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule,
-    MatSidenavModule, MatToolbarModule
+    MatSidenavModule, MatToolbarModule, MatListModule
   ]
 })
 export class MyMaterialModule {
